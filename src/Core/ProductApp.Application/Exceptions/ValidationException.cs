ad0138ebdbaee610179afa16bdc84e9d41a6da0f
zeynepozturkmen <zeynepozturkmen62@gmail.com>
2022-05-17 17:18:37 +0300

    Add project files.

---
+++ src/Core/ProductApp.Application/Exceptions/ValidationException.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductApp.Application.Exceptions
+{
+    public class ValidationException : Exception
+    {
+        public ValidationException():base("Validation error occurred")
+        {
+
+        }
+        public ValidationException(String Message):base(Message)
+        {
+
+        }
+        //this(ex.Message) -> ValidationException(String Message) bu ctor'u cagırıyor. 
+        //this(ex.Message) tıklayınca ValidationException(String Message) bu ctor'u isaretliyor
+        public ValidationException(Exception ex):this(ex.Message)
+        {
+
+        }
+    }
+}
---
+++ src/Core/ProductApp.Application/Wrappers/BaseResponse.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductApp.Application.Wrappers
+{
+    public class BaseResponse
+    {
+        public Guid Id { get; set; }
+        public bool IsSuccess { get; set; }
+        public String Message { get; set; }
+    }
+}
